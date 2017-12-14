fbe82d1d2ef191fc705f62ea12a3002a153c9d02
Bunnywabbit <needmorecarrots@gmail.com>
2017-12-14 16:55:40 +0100

    bugfix in rounding fnct call and
    implementation of 26px max font

--- global/fonts.lua
+++ global/fonts.lua
@@ -1,9 +1,9 @@
 --fonts
 Global = File.LoadLua("global/global.lua")()
 
-function Scaled(fontname, size, options)
+function create_scaled(fontname, size, options)
 	options = options or {}
-	Ltd_size = Number.Clamp(8, 25, Number.Round(size))
+	Ltd_size = Number.Clamp(8, 26, Number.Round(size,0))
 	b_bold = options.Bold or false
 	b_italic = options.Italic or false
 	b_underline = options.Underline or false 
@@ -14,10 +14,10 @@ end
 return {
 	p = 	Font.Create("Trebuchet MS", 18),
 	pbold =	Font.Create("Trebuchet MS", 18, {Bold=true}),
-	h1 = 	Font.Create("Trebuchet MS", 25, {Bold=true}),
+	h1 = 	Font.Create("Trebuchet MS", 26, {Bold=true}),
 	h2 = 	Font.Create("Trebuchet MS", 23, {Italic=true, Bold=true}),
 	h3 = 	Font.Create("Trebuchet MS", 21, {Bold=true}),
 	h4 = 	Font.Create("Trebuchet MS", 19, {Bold=true}),
-	Scaled = Scaled,
+	create_scaled = create_scaled,
 }
 	

3cc5bd9ecd038326b10db2a0867a55f76afb0882
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2018-06-22 10:28:40 +0200

    The configuration screen.

---
+++ menuconfig/configdata.lua
@@ -0,0 +1,237 @@
+
+Control = File.LoadLua("global/control.lua")()
+Button = File.LoadLua("button/button.lua")()
+
+local section_labels = {
+	Online="Online",
+	UI="User interface",
+	Graphics="Graphics",
+	Mouse="Mouse & Joystick",
+	Keybindings="Keybindings",
+	Sound="Sound",
+	Debug="Debug",
+}
+
+local section_order = {
+	"Online", "UI", 
+	"Graphics", 
+	"Mouse", "Keybindings", "Sound", "Debug"
+}
+
+function entry_to_label_with_none(entry)
+	return String.Switch(String.Equals(entry, ""), {
+		[ true ] = "[ None ]",
+		[ false ] = entry
+	})
+end
+
+function create_section_entries(context, create_configuration)
+	local section_entries = {
+		Graphics=function ()
+			-- need a dummy sink
+			local fullscreen_resolution_sink = Point.CreateEventSink(
+				Screen.Get("Configuration.Graphics.ResolutionX"),
+				Screen.Get("Configuration.Graphics.ResolutionY")
+			)
+
+			return {
+				create_configuration("Fullscreen", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.Fullscreen"), { true, false })),
+
+				create_configuration("Available fullscreen modes", Control.create_listbox(
+					fullscreen_resolution_sink,
+					Screen.Get("Fullscreen modes"),
+					{
+						entry_to_label=function (entry)
+							return entry:Get("Width") .. " x " .. entry:Get("Height") .. " @ " .. entry:Get("Rate")
+						end,
+						entry_to_value=function (entry)
+							return Point.Create(entry:Get("Width"), entry:Get("Height"))
+						end,
+						is_selected=function (a)
+							return Boolean.And(
+								Number.Equals(Screen.Get("Configuration.Graphics.ResolutionX"), Point.X(a)),
+								Number.Equals(Screen.Get("Configuration.Graphics.ResolutionY"), Point.Y(a))
+							)
+						end,
+						change_registration_callback=function (register)
+							register(Screen.Get("Configuration.Graphics.ResolutionX"), function (entry)
+								return entry:Get("Width")
+							end)
+							register(Screen.Get("Configuration.Graphics.ResolutionY"), function (entry)
+								return entry:Get("Height")
+							end)
+						end,
+					}
+				)),
+
+				create_configuration("Custom resolution", Image.Switch(Screen.Get("Configuration.Graphics.Fullscreen"), {
+					[ false ] = Image.StackHorizontal({
+						Control.int.create_input(context, Screen.Get("Configuration.Graphics.ResolutionX"), {
+							width = 50
+						}),
+						Image.String(Font.Create("Verdana", 16), Color.Create(0.8, 0.8, 0.8), "x"),
+						Control.int.create_input(context, Screen.Get("Configuration.Graphics.ResolutionY"), {
+							width = 50
+						}),
+					}, 10),
+					[ true ] = Image.String(Font.Create("Verdana", 16), Color.Create(0.8, 0.8, 0.8), "Not available in fullscreen mode"),
+				})),
+				
+
+				create_configuration("VSync", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.UseVSync"), { true, false })),
+				create_configuration("Anti-aliasing", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.UseAntiAliasing"), { true, false })),
+				create_configuration("Maximum texture size", Control.int.create_listbox(Screen.Get("Configuration.Graphics.MaxTextureSizeLevel"), { 0, 1, 2, 3 }, {
+					entry_to_label=function (entry)
+						local edge = Number.ToString(256 * Number.Power(2, entry), 0)
+						return edge .. "x" .. edge
+					end
+				})),
+
+				create_configuration("Brightness (gamma)", Control.number.create_input(context, Screen.Get("Configuration.Graphics.Gamma"))),
+
+				create_configuration("Render Environment", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.Environment"))),
+				create_configuration("Render Posters", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.Posters"))),
+				create_configuration("Render Stars", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.Stars"))),
+				create_configuration("Render Particles", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.Particles"))),
+				create_configuration("Render Lens Flare", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.LensFlare"))),
+
+				create_configuration("Debris", Control.number.create_listbox(Screen.Get("Configuration.Graphics.Debris"), { 0, 1.5, 1.0, 0.8 }, {
+					entry_to_label=function (entry)
+						return String.Switch(entry, {
+							[0] = "Off",
+							[1.5] = "Low",
+							[1.0] = "Medium",
+							[0.8] = "High",
+						}, Number.ToString(entry, 2))
+					end
+				})),
+			}
+		end,
+		Online=function ()
+			return {
+				create_configuration("Callsign", Control.string.create_input(context, Screen.Get("Configuration.Online.CharacterName"))),
+				create_configuration("Squad tag", Control.string.create_listbox(
+					Screen.Get("Configuration.Online.SquadTag"), 
+					Screen.Get("AvailableSquadTags"),
+					{
+						entry_to_label=entry_to_label_with_none,
+						change_registration_callback=function (register_func)
+							register_func(Screen.Get("Configuration.Online.OfficerToken"), function ()
+								return ""
+							end)
+						end
+					}
+				)),
+				create_configuration("Token", Control.string.create_listbox(
+					Screen.Get("Configuration.Online.OfficerToken"), 
+					Screen.Get("AvailableTokens"),
+					{
+						entry_to_label=entry_to_label_with_none
+					}
+				)),
+			}
+		end,
+		UI=function ()
+			return {
+				create_configuration("Startup credits", Control.boolean.create_listbox(Screen.Get("Configuration.Ui.ShowStartupCreditsMovie"))),
+				create_configuration("Startup movie", Control.boolean.create_listbox(Screen.Get("Configuration.Ui.ShowStartupIntroMovie"))),
+				create_configuration("Startup training suggestion", Control.boolean.create_listbox(Screen.Get("Configuration.Ui.ShowStartupTrainingSuggestion"))),
+				create_configuration("Use old UI", Control.boolean.create_listbox(Screen.Get("Configuration.Ui.UseOldUi"))),
+
+				create_configuration("Chat number of lines", Control.int.create_listbox(Screen.Get("Configuration.Chat.NumberOfLines"), { 5, 6, 7, 8, 9, 10})),
+				create_configuration("Censor chat", Control.boolean.create_listbox(Screen.Get("Configuration.Chat.CensorChat"))),
+				create_configuration("Filter chats to all", Control.boolean.create_listbox(Screen.Get("Configuration.Chat.FilterChatsToAll"))),
+				create_configuration("Filter voice chats", Control.boolean.create_listbox(Screen.Get("Configuration.Chat.FilterVoiceChats"))),
+				create_configuration("Filter chats from lobby", Control.boolean.create_listbox(Screen.Get("Configuration.Chat.FilterChatsFromLobby"))),
+				create_configuration("Filter unknown chats", Control.boolean.create_listbox(Screen.Get("Configuration.Chat.FilterUnknownChats"))),
+
+
+				create_configuration("Hud Style", Control.int.create_listbox(Screen.Get("Configuration.Ui.HudStyle"), { 0, 1, 2, 3, 4 }, {
+					entry_to_label=function (entry)
+						return String.Switch(entry, {
+							[0] = "Normal",
+							[1] = "Software",
+							[2] = "Custom Hud 1",
+							[3] = "Custom Hud 2",
+							[4] = "Custom Hud 3",
+						}, Number.ToString(entry))
+					end
+				})),
+			}
+		end,
+		Mouse=function ()
+			return {
+				create_configuration("Use mouse as joystick", Control.boolean.create_listbox(Screen.Get("Configuration.Joystick.UseMouseAsJoystick"))),
+				create_configuration("Show direction indicator", Control.boolean.create_listbox(Screen.Get("Configuration.Joystick.ShowDirectionIndicator"))),
+				
+				create_configuration("Mouse sensitivity", Control.number.create_input(context, Screen.Get("Configuration.Mouse.Sensitivity"), {
+					decimals=2
+				})),
+				create_configuration("Mouse acceleration", Control.int.create_listbox(Screen.Get("Configuration.Mouse.Acceleration"), { 0, 1, 2 }, {
+					entry_to_label=function (entry)
+						return String.Switch(entry, {
+							[0] = "[ none ]",
+							[1] = "2x",
+							[2] = "4x",
+						}, Number.ToString(entry))
+					end
+				})),
+
+				create_configuration("Flip Y Axis", Control.boolean.create_listbox(Screen.Get("Configuration.Joystick.FlipYAxis"))),
+
+				create_configuration("Joystick control response", Control.boolean.create_listbox(Screen.Get("Configuration.Joystick.ControlsLinear"), {
+					entry_to_label=function (entry)
+						return String.Switch(entry, {
+							[true] = "Linear",
+							[false] = "Quadratic",
+						})
+					end
+				})),
+				create_configuration("Joystick deadzone", Control.int.create_listbox(Screen.Get("Configuration.Joystick.Deadzone"), { 4, 10, 30 }, {
+					entry_to_label=function (entry)
+						return String.Switch(entry, {
+							[4] = "Smallest",
+							[10] = "Small",
+							[30] = "Largest",
+						}, Number.ToString(entry))
+					end
+				})),
+				create_configuration("Joystick force feedback", Control.boolean.create_listbox(Screen.Get("Configuration.Joystick.EnableForceFeedback"))),
+			}
+		end,
+		Keybindings=function ()
+			local button = Button.create_standard_textbutton("Open keybindings popup", Font.Create("Verdana", 18), 200, 50)
+			Event.OnEvent(Screen.Get("Open keymap popup"), button.events.click)
+			return {
+				button.image
+			}
+		end,
+		Sound=function ()
+			return {
+				create_configuration("Effect volume", Control.int.create_input(context, Screen.Get("Configuration.Sound.EffectVolume"))),
+				create_configuration("Voice over volume", Control.int.create_input(context, Screen.Get("Configuration.Sound.VoiceVolume"))),
+			}
+		end,
+		Debug=function ()
+			return {
+				create_configuration("Artwork path", Control.string.create_input(context, Screen.Get("Configuration.Data.ArtworkPath"))),
+
+				create_configuration("Log to file", Control.boolean.create_listbox(Screen.Get("Configuration.Debug.LogToFile"))),
+				create_configuration("Log to output", Control.boolean.create_listbox(Screen.Get("Configuration.Debug.LogToOutput"))),
+				create_configuration("Log Mdl", Control.boolean.create_listbox(Screen.Get("Configuration.Debug.Mdl"))),
+				create_configuration("Window logging", Control.boolean.create_listbox(Screen.Get("Configuration.Debug.Window"))),
+				create_configuration("Debugging mode LUNA", Control.boolean.create_listbox(Screen.Get("Configuration.Debug.Lua"))),
+
+				create_configuration("Render bounding boxes", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.BoundingBoxes"))),
+				create_configuration("Render objects transparent", Control.boolean.create_listbox(Screen.Get("Configuration.Graphics.TransparentObjects"))),
+			}
+		end,
+	}
+	return section_entries
+end
+
+return {
+	section_labels=section_labels,
+	section_order=section_order,
+	create_section_entries=create_section_entries
+}
---
+++ menuconfig/configscreen.lua
@@ -0,0 +1,197 @@
+
+Context = File.LoadLua("global/context.lua")()
+Button = File.LoadLua("button/button.lua")()
+Global = File.LoadLua("global/global.lua")()
+Control = File.LoadLua("global/control.lua")()
+
+config_data = File.LoadLua("menuconfig/configdata.lua")()
+
+context = Context.create_context()
+
+resolution = Screen.GetResolution()
+resolution_x = Point.X(resolution)
+resolution_y = Point.Y(resolution)
+
+titlebar_container_height = 100
+titlebar_container_size = Point.Create(resolution_x, titlebar_container_height)
+
+titlebar = Image.Group({
+	Image.Extent(titlebar_container_size, Color.Create(0, 0, 0.5)),
+	Image.Justify(
+		Image.String(Font.Create("Verdana", 60), Color.Create(0.8, 0.8, 0.8), "Configuration"),
+		titlebar_container_size,
+		Justify.Center
+		)
+	})
+
+local sidebar_width = 200;
+local sidebar_height = resolution_y - titlebar_container_height
+local sidebar_size = Point.Create(sidebar_width, sidebar_height)
+
+
+local current_selected_tab = String.CreateEventSink("")
+
+
+function create_sidebar_buttons_container()
+	local button_font = Font.Create("Verdana", 16)
+	local button_width = 150
+	local button_height = 30
+	local button_separation = 10
+
+	function create_button_image(sink, label)
+		local button = Button.create_standard_textbutton(label, button_font, button_width, button_height)
+		Event.OnEvent(sink, button.events.click)
+		return button.image
+	end
+
+	local sidebar_buttons_container = Image.StackVertical({
+		create_button_image(Screen.Get("Open help popup"), "Quick reference"),
+		Image.Switch(Screen.Get("Is in mission"), {
+			[ true ] = Image.StackVertical({
+				create_button_image(Screen.Get("Open mission info popup"), "Mission info"),
+				create_button_image(Screen.Get("Open ping popup"), "Player pings"),
+			}, button_separation)
+		}),
+		create_button_image(Screen.Get("Close sink"), "Close and continue"),
+		Image.Switch(Screen.Get("Is in mission"), {
+			[ true ] = create_button_image(Screen.Get("Quit mission sink"), "Quit mission")
+		}),
+		create_button_image(Screen.Get("Quit allegiance sink"), "Quit Allegiance"),
+	}, button_separation)
+
+	local sidebar_button_container_height = Point.Y(Image.Size(sidebar_buttons_container)) + 20
+
+	return Image.Group({
+		Image.Extent(Point.Create(sidebar_width - 20, sidebar_button_container_height), Color.Create(0, 0, 0.5)),
+		Image.Justify(sidebar_buttons_container, Point.Create(sidebar_width - 20, sidebar_button_container_height), Justify.Center)
+	}), sidebar_button_container_height
+end
+
+local sidebar_buttons_container, sidebar_button_container_height = create_sidebar_buttons_container()
+
+local sidebar_sections_height = sidebar_height - sidebar_button_container_height
+
+function create_sections_container()
+
+	local button_size = Point.Create(sidebar_width, 50)
+
+	local button_background = Image.Extent(button_size, Color.Create(0, 0, 0.3))
+	local button_background_selected = Image.Extent(button_size, Color.Create(0, 0, 0.5))
+
+	local section_images = {}
+	for i, key in pairs(config_data.section_order) do
+		local label = config_data.section_labels[key]
+
+		function create_button_image(background_image, label)
+			return Image.Group({
+				background_image,
+				Image.Justify(
+					Image.String(Font.Create("Verdana", 16), Color.Create(0.8, 0.8, 0.8), label),
+					Image.Size(background_image),
+					Justify.Center
+				)
+			})
+		end
+		local section_button = Button.create_image_button(
+			create_button_image(
+				Image.Switch(current_selected_tab, {
+					[key]=button_background_selected
+				}, button_background),
+				label
+			),
+			create_button_image(
+				button_background_selected,
+				label
+			)
+		)
+
+		Event.OnEvent(current_selected_tab, section_button.events.click, function ()
+			return key
+		end)
+		section_images[#section_images+1] = Image.Group({
+			section_button.image
+		})
+	end
+
+	local sidebar_sections_container = Image.StackVertical(section_images, 20)
+
+	return Global.create_vertical_scrolling_container(
+		sidebar_sections_container, 
+		Point.Create(sidebar_width, sidebar_sections_height), 
+		Color.Create(1, 0, 0)
+	)
+end
+
+
+local sidebar_sections_container = create_sections_container()
+
+local sidebar = Image.Group({
+	Image.Extent(sidebar_size, Color.Create(0, 0, 0.2)),
+	Image.Translate(sidebar_sections_container, Point.Create(0, 10)),
+	Image.Justify(sidebar_buttons_container, sidebar_size, Justify.Bottom)
+})
+
+
+local divider_position = 200
+local divider_margin = 10
+function create_configuration(label, configuration_image)
+	local label_font = Font.Create("Verdana", 16)
+	local label_image = Image.String(label_font, Color.Create(0.8, 0.8, 0.8), label)
+
+	local configuration_image_height = Point.Y(Image.Size(configuration_image))
+	local label_image_height = Point.Y(Image.Size(label_image))
+
+	local configuration_entry_height = Number.Max(configuration_image_height, label_image_height)
+
+	return Image.Group({
+		Image.Justify(label_image, Point.Create(divider_position - divider_margin, configuration_entry_height), Justify.Right),
+		Image.Extent(Rect.Create(divider_position - 1, 0, divider_position + 1, configuration_entry_height), Color.Create(0.5, 0.5, 0.5)),
+		Image.Translate(
+			configuration_image,
+			Point.Create(divider_position + divider_margin, 0)
+		)
+		
+	})
+end
+
+local section_entries = config_data.create_section_entries(context, create_configuration)
+
+function create_contents_container()
+	local content_container_size = Point.Create(resolution_x - sidebar_width, sidebar_height)
+
+	local lazy_sections = {}
+	for key, config_func in pairs(section_entries) do
+		lazy_sections[key] = Image.Lazy(function ()
+			local configuration_entry_images = config_func()
+
+			local content_image = Image.Group({
+				Image.Translate(
+					Image.StackVertical(configuration_entry_images, 20),
+					Point.Create(0, 20)
+				)
+			})
+
+			return Global.create_vertical_scrolling_container(content_image, content_container_size, Color.Create(1, 0, 0))
+		end)
+	end
+
+	return Image.Group({
+		Image.Extent(content_container_size, Color.Create(0, 0, 0)),
+		Image.Switch(current_selected_tab, lazy_sections)
+	})
+end
+local contents_container = create_contents_container()
+
+return Image.Group({
+	titlebar,
+	Image.Translate(
+		Image.Group({
+			sidebar,
+			Image.Translate(
+				contents_container,
+				Point.Create(sidebar_width, 0)
+			)			
+		}),
+		Point.Create(0, titlebar_container_height)
+	),
+})
